a3defd4b2d131b04e7b55355abff52a02939da3f
mallickraja1909 <mallickraja1909@gmail.com>
2022-04-28 05:44:55 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import {FaBars,FaTimes,FaGithub,FaLinkedin,FaFacebook} from 'react-icons/fa';
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import { Link } from 'react-scroll/modules';
+// import Contact from './Contact';
 const Navbar = () => {
   const [nav,setNav]=useState(false)
   const handleClick = ()=> setNav(!nav)
@@ -85,34 +86,35 @@ const Navbar = () => {
      </li>
 
      </ul>
+     
 {/* sociial icon linbks */}
 <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
 
 <ul>
   <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-0px] bg-blue-500'>
     <a className='flex justify-between items-center w-full text-gray-300' 
-    href="/">
+    href="https://www.facebook.com/profile.php?id=100007839015581">
 Facebook<FaFacebook size={30}/>
     </a>
   </li>
 {/*  */}
 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-0px] bg-gray-700'>
     <a className='flex justify-between items-center w-full text-gray-300' 
-    href="/">
+    href="https://github.com/mallickraja1909">
 Github<FaGithub size={30}/>
     </a>
   </li>
   {/*  */}
   <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-0px] bg-blue-700'>
     <a className='flex justify-between items-center w-full text-gray-300' 
-    href="/">
+    href="https://www.linkedin.com/in/raja-mallick-2b62a0131/">
 Linkedin<FaLinkedin size={30}/>
     </a>
   </li>
   {/*  */}
   <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-0px] bg-[#6fc2b0]'>
     <a className='flex justify-between items-center w-full text-gray-300' 
-    href="/">
+    href='URL={../D:\j-rjs\portfolio-react-app\src\components\Contact.jsx}'>
 Mail<HiOutlineMail size={30}/>
     </a>
   </li>
